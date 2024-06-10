2b009073e556c393df8972e864f6da759d673148
olyvka <oliamagir06@gmail.com>
2024-06-10 18:04:42 +0300

    Card generate

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -8,11 +8,15 @@ export default function ImageCard({
     onClick(urls.regular, alt_description, likes, user.name);
   };
 
+  // Визначаємо значення src та alt з урахуванням умови
+  const srcValue = urls ? urls.small : '';
+  const altValue = alt_description ? alt_description : '';
+
   return (
     <div className={css.box}>
       <img
-        src={urls.small}
-        alt={alt_description}
+        src={srcValue}
+        alt={altValue}
         onClick={handleClick}
         className={css.img}
       />
